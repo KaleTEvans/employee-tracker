616177d0d05cdb03bd6dc42409bd15c83c9380cc
KaleTEvans <kaletevans@gmail.com>
2021-05-03 14:48:39 -0500

    added add role and add department capability

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const cTable = require('console.table');
+const { response } = require('express');
+
 
 const pathWay = 'http://localhost:3001/api';
+const departmentArray = [];
 
 const startApp = () => {
     console.log(
@@ -31,13 +34,70 @@ const startApp = () => {
             return employeeView();
         }
         if (optionSelect === 'Add a department') {
-            return departmentAdd();
+            inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'departmentName',
+                    message: 'Enter the department name'
+                }
+            ]).then(departmentName => {
+                departmentAdd(departmentName);
+            });
         }
         if (optionSelect === 'Add a role') {
-            return rollAdd();
+            departmentList();
+            inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'roleName',
+                    message: 'Enter the name of the role',
+                    validate: roleName => {
+                        if (roleName) {
+                            return true;
+                        } else {
+                            console.log('Please enter a valid role name');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'number',
+                    name: 'roleSalary',
+                    message: 'Enter the salary of the role',
+                    validate: roleSalary => {
+                        if (roleSalary) {
+                            return true;
+                        } else {
+                            console.log('Please enter a valid salary');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'departmentSelect',
+                    message: "Select the role's department",
+                    choices: departmentArray
+                }
+            ]).then(({ roleName, roleSalary, departmentSelect }) => {
+                let departmentId;
+                departmentArray.forEach(department => {
+                    if (departmentSelect === department.name) {
+                        departmentId = department.id;
+                    }
+                })
+                let newSalary = JSON.stringify(roleSalary);
+                roleAdd(roleName, newSalary, departmentId);
+            })
         }
         if (optionSelect === 'Add an employee') {
-            return employeeAdd();
+            inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'employeeName',
+                    message: 'Enter the name of the employee'
+                }
+            ])
         }
         if (optionSelect === 'Update an employee role') {
             return employeeUpdate();
@@ -81,27 +141,80 @@ const roleView = () => {
     fetch(`${pathWay}/roles`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'applocation/json',
+            'Content-Type': 'application/json',
         },
     }).then(res => res.json())
     .then(json => {
-        
         console.table(json.data);
     })
     .then(portalFunction);
-}
+};
 
 const employeeView = () => {
     fetch(`${pathWay}/employees`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'applocation/json',
+            'Content-Type': 'application/json',
         },
     }).then(res => res.json())
     .then(json => {
         console.table(json.data);
     })
     .then(portalFunction);
+};
+
+const departmentAdd = (departmentName) => {
+    (async () => {
+        const rawResponse = await fetch(`${pathWay}/department`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: departmentName.departmentName })
+        });
+        const content = await rawResponse.json();
+
+        console.log(`${content.data.name} added to Departments.`);
+    })().then(portalFunction)
+};
+
+const roleAdd = (roleName, roleSalary, departmentId) => {
+    console.log(roleName, roleSalary, departmentId);
+    (async () => {
+        const rawResponse = await fetch(`${pathWay}/role`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                title: roleName,
+                salary: roleSalary,
+                department_id: departmentId
+            })
+        });
+        const content = await rawResponse.json();
+
+        console.log(`${content.data.title} added to Roles`);
+    })().then(portalFunction)
+};
+
+
+// functions to acquire lists for selection
+const departmentList = () => {
+    fetch(`${pathWay}/departments`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(res => res.json())
+    .then(json => {
+        json.data.forEach(department => {
+            departmentArray.push(department);
+        });
+    });
 }
 
 module.exports = { startApp };
--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 // get all departments
 router.get('/departments', (req, res) => {
@@ -16,5 +17,45 @@ router.get('/departments', (req, res) => {
         });
     });
 });
+
+// add a department
+router.post('/department', ({ body }, res) => {
+
+    const sql = `INSERT INTO departments (name)
+                    VALUES (?)`;
+    const params = body.name;
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Department not found!'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+})
 
 module.exports = router;
